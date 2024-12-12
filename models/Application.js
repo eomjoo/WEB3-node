a338d51f74055481a8cc9dfb2256d397aa240395
jesuszero <djawndud5923@naver.com>
2024-12-12 21:07:27 +0900

    지원 모델

--- models/Application.js
+++ models/Application.js
@@ -8,19 +8,10 @@ class Application {
     db.query(sql, [userId, jobId], callback);
   }
 
-  // 사용자별 지원 목록 조회
-  static getUserApplications(userId, status, callback) {
-    let sql = 'SELECT * FROM Applications WHERE user_id = ?';
-    const params = [userId];
-
-    if (status) {
-      sql += ' AND status = ?';
-      params.push(status);
-    }
-
-    sql += ' ORDER BY applied_at DESC';
-
-    db.query(sql, params, callback);
+   // 사용자 ID로 지원한 공고 목록 조회
+   static getUserApplicationsById(userId, callback) {
+    const sql = 'SELECT job_id FROM Applications WHERE user_id = ? ORDER BY applied_at DESC';
+    db.query(sql, [userId], callback);
   }
 
   // 지원 정보 삭제 (취소)
